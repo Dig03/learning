c48ffa8817052c8b8e6e55e42df4051e6116ee3a
Dig <Dig03@users.noreply.github.com>
2019-08-14 21:09:11 +0100

    Progress on Haskell through modules chapter.

--- hs/learn-you/6-modules.hs
+++ hs/learn-you/6-modules.hs
@@ -50,4 +50,50 @@ uncaesar n = caesar (negate n)
 caesarId :: Int -> String -> String
 caesarId n = (uncaesar n . caesar n)
 
-{- BOOKMARK: PAGE 93 -}
+-- Strict Left Folds
+{-
+    Sometimes we can cause a stack overflow due to Haskell's
+    lazy evaluation.
+
+    This causes the stack to be blown because Haskell defers
+    the evaluation of a massive number of folds and embeds
+    them all in the callstack.
+-}
+
+-- this isn't causing a stack overflow on my machine, but
+-- excessively large values likely could
+stackoverflow = foldl (+) 0 (replicate 1000000 1)
+
+-- Data.List provides foldl', a 'strict' foldl
+-- called such because it _strictly_ evaluates, no deferring
+-- (same for foldl1 and foldl1')
+
+-- digitToInt is from Data.Char
+digitSum :: Int -> Int
+digitSum = sum . map digitToInt . show
+
+firstDigitSumTo40 :: Int
+firstDigitSumTo40 = 1 + (length . takeWhile (< 40) $ map digitSum [1..])
+
+-- a better alternative is 'find' from Data.List
+-- find returns the first element meeting some predicate
+-- however it returns it as a Maybe
+-- Maybe is used to represent possible failure
+-- Something of type Maybe is either Just a
+-- or Nothing
+
+firstDigitSumTo40' :: Maybe Int
+firstDigitSumTo40' = find ((== 40) . digitSum) [1..]
+
+firstDigitSumTo :: Int -> Maybe Int
+firstDigitSumTo n = find ((== n) . digitSum) [1..]
+
+fancyFirstDigitSumTo :: Int -> Integer
+fancyFirstDigitSumTo n = read (intToDigit (n `mod` 9) : replicate (n `div` 9) '9') :: Integer
+
+abbrevFancyFirstDigitSumTo :: Int -> (Int, Int)
+abbrevFancyFirstDigitSumTo n = (n `mod` 9, n `div` 9)
+
+-- mapping keys to values
+
+{- BOOKMARK: PAGE 98 -}
